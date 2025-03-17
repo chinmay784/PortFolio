0501cd084134e06fb1d7f1447bdd083f8a6e3fbf
Chinmay Puhan <122338728+chinmay784@users.noreply.github.com>
2025-03-17 19:13:46 +0530

    Update constants.js
    Update

--- src/data/constants.js
+++ src/data/constants.js
@@ -9,7 +9,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/chinmay784",
   resume:
-    "https://drive.google.com/file/d/1txFKZD3LNIXMc329jD9c995AU8LItOXK/view?usp=drive_link",
+    "https://drive.google.com/file/d/1t4rcKWE_CEwk9vnKiR88tztdKJFUDRLH/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/chinmay-puhan-98ab41258/",
   git: "https://github.com/chinmay784",
   insta: "https://www.instagram.com/_its_chinmay_84/",
